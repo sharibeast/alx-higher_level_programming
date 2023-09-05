459d0397777abb18f756f9b57db7750e8f688679
sharibeast <georginasharif@gmail.com>
2023-09-06 00:17:58 +0300

    adding print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for number in range(0, 10):
     for m in range(number+1, 10):
         if (not (n == 8 and m == 9)):
-            print("{}{}".format(n,m), end=",")
+            print("{}{}".format(n, m), end=",")
         else:
             print("{}{}".format(number, m))
-            
