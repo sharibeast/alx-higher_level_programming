ff353e12672c21068c968dc7024a111cb32140b2
sharibeast <georginasharif@gmail.com>
2023-09-16 09:04:56 +0300

    function that replace all occurrences of an element

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(mylist,search,replace):
+    created_list = []
+    for x in mylist:
+        if x == search:
+            created_list.append(replace)
+        else:
+            created_list.append(x)
+    return created_list
+
