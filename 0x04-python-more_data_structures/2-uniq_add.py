c86c271b4e99789a090dcd0fb51be03cf501db56
sharibeast <georginasharif@gmail.com>
2023-09-16 09:11:39 +0300

    uniq add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    for number in new_list:
+        if x not in new_list:
+            new_list.append(x)
+        return sum(new_list)
