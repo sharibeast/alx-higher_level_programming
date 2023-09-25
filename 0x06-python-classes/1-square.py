a5f9040a21fcf4337e19b6fe30f96a70318ea7da
sharibeast <georginasharif@gmail.com>
2023-09-26 01:55:22 +0300

    adding 1 square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" create a square class """
+
+
+class Square:
+    """ clas Square definition with private size instance """
+    def __init__(self, size):
+        this.__size = size
