3058dc6d3bde49bab9243d85aee293d90a2ad476
sharibeast <georginasharif@gmail.com>
2023-09-26 02:22:04 +0300

    fx:adding 2 square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Define a class square """
+
+
+class Square:
+    """ clas Square definition with private size instance """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+        this.__size = size
