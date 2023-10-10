00805b8dd78f1db4fc67c30f98ccc62a25c3cb53
sharibeast <georginasharif@gmail.com>
2023-10-10 03:07:52 +0300

    my int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
-"""Definition  class MyInt that inherits int"""
+"""
+with a class Myint
+"""
 
 
 class MyInt(int):
-    """Inherits int operators == and !=."""
+    """class that inherits from int"""
 
-    def __eq__(self, value):
-        """Override == opertor with != behvior."""
-        return self.real != value
+    def __init__(self, my_int):
+        """for initialize a value my_int"""
 
-    def __ne__(self, value):
-        """Override != operator with == behavior. """
-        return self.real == value
+        self.my_int = my_int
+
+    @property
+    def my_int(self):
+        return self.__my_int
+
+    @my_int.setter
+    def my_int(self, my_int):
+        if type(my_int) is not int:
+            raise TypeError("my_int must be an integer")
+        else:
+            self.__my_int = my_int
+
+    def __eq__(self, other):
+        """equal method"""
+
+        if self.my_int == other:
+            return False
+        else:
+            return True
+
+    def __ne__(self, other):
+            if self.my_int != other:
+                return False
+            else:
+                return True
