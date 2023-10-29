42257372dcc6c4525c9b1a07171e37a47e44edcd
sharibeast <georginasharif@gmail.com>
2023-10-29 23:28:16 +0300

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function to read a text file (utf8) and print to stdout
+"""
+
+def read_file(filename):
+    """
+    A functon to read a text file (utf8) and print to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+# alx-higher_level_programming
+
+Alx Higher level programming
